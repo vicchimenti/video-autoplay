bf786873c1f95155664374599db0e88ec14e44e6
Vic Chimenti <victor.chimenti@gmail.com>
2023-01-14 12:22:22 -0800

    target style

--- custom-control-script.js
+++ custom-control-script.js
@@ -1,1 +1,19 @@
-document.getElementById('zoneA').getElementsByClassName('ControlBar_module_controlBarWrapper__278effbc')[0];
+
+document.addEventListener("DOMContentLoaded", () => {
+    let playerFrame = document.getElementById("player");
+    let playerDetail = playerFrame.contentDocument;
+    let target = playerDetail.getElementById('player').getElementsByClassName('ControlBar_module_controlBarWrapper__278effbc')[0];
+    target.style.padding = "0 0 300px 0";
+});
+
+function myFunction() {
+  var x = document.getElementById("myframe");
+  var y = x.contentDocument;
+  y.body.style.backgroundColor = "red";
+}
+
+
+document.getElementById('player').getElementsByClassName('ControlBar_module_controlBarWrapper__278effbc')[0];
+
+
+<script></script>
